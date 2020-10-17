bfb2dc63d743fd9d4b8f3827819438c5e46d05b8
santhoshshanmugham <santhosh.ind@gmail.com>
2020-10-17 21:05:19 +0530

    Pushing files from local repository to cloud repository first time

---
+++ test/f6.py
@@ -0,0 +1,20 @@
+GIT COMMANDS:
+
+For setup
+git config --global user.name User_name_in_quotation_if_space_in_name
+git config --global user.email User_email_id
+git config --global core.autocrlf true
+there is a different command for Mac
+git config --global --list (to show if the setup is done)
+
+git clone Repo_link => This will clone the repository from Cloud to your local
+git status => to check the status of the repository
+git add Dir_name/FileName_with_extension => Move the file to Staging Area
+git commit -m 'msg' - move the file to local repository with the msg
+git push - push the code to cloud repo (Will ask for credential while doing first time)
+git pull - pulling all the changes from your cloud repo to local repo
+
+FILE RELATED OPERATIONS
+To Open a file from CLI - vim filename.ext
+
+
